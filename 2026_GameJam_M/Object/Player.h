736957ddadafcaca48992ce93f4efa678b77b3dd
karupaltutyo <138739785+karupaltutyo@users.noreply.github.com>
2026-02-20 09:50:42 +0900

    player
    あ

---
+++ 2026_GameJam_M/Object/Player.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "../Utility/Vector2D.h"
+
+class Player
+{
+public:
+
+	Vector2D location;  //位置座標
+	Vector2D velocity;  //移動量
+	float g_velocity =0.0f //重力速度
+
+
+
+};
+
--- 2026_GameJam_M/Object/Player/Player.h
+++ 2026_GameJam_M/Object/Player/Player.h
@@ -1,15 +1,25 @@
 #pragma once
 
-#include "../Utility/Vector2D.h"
+#include "../../Utility/Vector2D.h"
 
 class Player
 {
 public:
-
-	float gravity;
-	float jump;
-
-
+	Vector2D location;		//位置座標
+	Vector2D velocity;		//移動量
+	Vector2D box_size;		//矩形の大きさ
+	float g_velocity;		//重力速度
+public:
+	Player();
+	~Player();
+public:
+	void Initialize();
+	void Update(float delta_second);
+	void Draw() const;
+	void Finalize();
+public:
+	void Movement(float param);
+	void Jumpmovement(float param);
 
 };
 
